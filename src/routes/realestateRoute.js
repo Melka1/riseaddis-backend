d17e8f164a0f5a0ede9b6b6cdb7b240007e23549
Melka Tolessa <63374554+Melka1@users.noreply.github.com>
2024-06-12 09:38:45 +0300

    chore name correction

--- src/routes/realestateRoute.js
+++ src/routes/realestateRoute.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 
 import addRealestate from "../controllers/realestateController/addRealestate.js";
-import updateRealestate from "../controllers/realestateController/updateRealEstate.js";
-import deleteRealestate from "../controllers/realestateController/deleteRealEstate.js";
-import getRealEstates from "../controllers/realestateController/getRealEstates.js";
-import getRealEstate from "../controllers/realestateController/getRealEstate.js";
+import updateRealestate from "../controllers/realestateController/updateRealestate.js";
+import deleteRealestate from "../controllers/realestateController/deleteRealestate.js";
+import getRealEstates from "../controllers/realestateController/getRealestates.js";
+import getRealEstate from "../controllers/realestateController/getRealestate.js";
 import getAllRealEstate from "../controllers/realestateController/getAllRealEstate.js";
 import getRealEstateList from "../controllers/realestateController/getRealEstateList.js";
 import adminAuthentication from "../middlewares/AdminAuthentication.js";
